4cd59284aa7f381d8a05d4e037a3cb795caebec2
LogCreative <longlaoda123456@qq.com>
2020-10-08 11:10:38 +0800

    online compiler: aslushnikov/latex-online

--- web/js/main.js
+++ web/js/main.js
@@ -1,25 +1,42 @@
 var app = new Vue({
     el: '#app',
     data:{
-        series: null,
+        series: "",
         axis: null,
-        param: null
+        param: "",
+        premable:"\\documentclass[tikz]{standalone}\n\
+\\usepackage{pgfplots}\n\
+\\pgfplotsset{compat=1.14}\n\
+\\begin{document}\n\
+\\begin{tikzpicture}",
+        suffix:"\\end{tikzpicture}\n\
+\\end{document}",
+        curl:"",
+    },
+    computed:{
+        content: function(){
+            return "\\begin{axis}["+this.param+"]\n "+this.series+"\n\\end{axis}";
+        },
+        file: function(){
+            return this.premable+this.content+this.suffix;
+        },
     },
     methods:{
         onFunctionEnter: function(){
             app.series = "Funtion";
         },
         onFunctionLeave: function() {
-            app.series = null;
+            app.series = "";
         },
         onPlotEnter: function() {
             app.series = "Plot";
         },
         onPlotLeave: function() {
-            app.series = null;
+            app.series = "";
         },
         compile: function() {
-            
+            // 清除换行无效符号
+            app.curl = "https://latexonline.cc/compile?text="+this.file.replace(/[\r\n]/g,"");
         }
     }
 });
