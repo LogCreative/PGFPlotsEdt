6d9f645378168162d5bfdc1b4dbd30116a2b4008
LogCreative <longlaoda123456@qq.com>
2021-03-05 10:07:33 +0800

    fix get version

--- js/getversion.js
+++ js/getversion.js
@@ -9,15 +9,15 @@
                 //code for Internet Explorer
                 xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
             }
+            var mainver = 1;            // Manual Modification over the main ver.
             xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                     var repo = JSON.parse(xmlhttp.responseText);
-                    // FIX ME
-                    document.getElementById("version").innerHTML = 'v' + (Object.keys(repo).length / 100).toFixed(2);
+                    document.getElementById("version").innerHTML = 'v' + (mainver + Object.keys(repo).length / 100).toFixed(2);
                 } else {
                 }
             };
-            xmlhttp.open('GET', 'https://api.github.com/repos/' + repo +'/commits?per_page=1000', true);            //Up to 1000
+            xmlhttp.open('GET', 'https://api.github.com/repos/' + repo +'/commits?per_page=100&page=' + (mainver + 1), true);            // Up to 100
             xmlhttp.send();
         }
         _ajaxReq("LogCreative/PGFPlotsEdt");
