8759d0cb4f67cba7cb21bd678c4026bf32e6712d
LogCreative <longlaoda123456@qq.com>
2021-04-08 13:09:21 +0800

    anim svg frame

--- res/logo/anim/animrender.ps1
+++ res/logo/anim/animrender.ps1
@@ -5,29 +5,34 @@
 Remove-Item *.dvi, *.log, *.aux, animatedlogo-frame*.tex, *.svg
 
 # Powershell 7 Only -- to render in Parallel
-0..22 | ForEach-Object -Parallel {
-    $view_replacer = "view={"+ ($_ * 2) +"}"
-    $domain_start = -0.57 - ($_ + 1) * 0.04
-    $domain_end = -0.43 + ($_ + 1) * 0.04
+0..45 | ForEach-Object -Parallel {
+    $view_replacer = "view={"+ $_ +"}"
+    $domain_start = -0.57 - ($_ + 1) * 0.02
+    $domain_end = -0.43 + ($_ + 1) * 0.02
     $domain_replacer = "domain=" + $domain_start + "*pi" + ":" + $domain_end + "*pi"
-    $file = "animatedlogo-frame" + $_ + ".tex"
+    $index = '{0:d2}' -f $_
+    $file = "animatedlogo-frame" + $index + ".tex"
     (Get-Content ../logo.tex -Raw) -replace "view={(\d+)}", $view_replacer -replace "domain=0:2\*pi", $domain_replacer | 
         Out-File $file
-    $dvifile = "animatedlogo-frame" + $_ + ".dvi"
+    $dvifile = "animatedlogo-frame" + $index + ".dvi"
     pdflatex -output-format=dvi $file
     dvisvgm --zoom=1 --exact $dvifile
 }
 
-# need ImageMagick installed
-magick -size 640x640 -depth 8 -loop 1 animatedlogo-frame%d.svg[0-22] animatedlogo.gif 
+# svg only mode
 
-# backward
+# # need ImageMagick installed
+# magick -size 640x640 -depth 8 -loop 1 animatedlogo-frame%d.svg[0-22] animatedlogo.gif 
 
-0..22 | ForEach-Object {
-    $filename = "animatedlogo-frame" + $_ + ".svg"
-    $index = 45 - $_
-    $new_filename = "animatedlogo-frame" + $index + ".svg"
-    Rename-Item $filename $new_filename
-}
+# # backward
 
-magick -size 640x640 -depth 8 -loop 1 animatedlogo-frame%d.svg[23-45] animatedlogo_back.gif 
+# 0..22 | ForEach-Object {
+#     $filename = "animatedlogo-frame" + $_ + ".svg"
+#     $index = 45 - $_
+#     $new_filename = "animatedlogo-frame" + $index + ".svg"
+#     Rename-Item $filename $new_filename
+# }
+
+# magick -size 640x640 -depth 8 -loop 1 animatedlogo-frame%d.svg[23-45] animatedlogo_back.gif 
+
+Remove-Item *.dvi, *.log, *.aux, animatedlogo-frame*.tex
