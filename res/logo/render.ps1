50dc2391938ed4968ee8c544dc97f8d85abaf4e3
LogCreative <longlaoda123456@qq.com>
2021-04-08 22:44:19 +0800

    favicon modified

--- res/logo/render.ps1
+++ res/logo/render.ps1
@@ -1,3 +1,5 @@
 pdflatex -output-format=dvi logo.tex
 dvisvgm --zoom=-1 --exact --font-format=woff logo.dvi
+pdflatex -output-format=dvi logoicon.tex
+dvisvgm --zoom=-1 --exact --font-format=woff logoicon.dvi
 Remove-Item *.aux, *.dvi, *.log
