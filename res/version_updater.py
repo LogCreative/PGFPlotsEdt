e124e4f86a615c708f93dba590972b042d31e79c
LogCreative <logcreative@outlook.com>
2024-03-25 12:09:40 +0800

    fix: version date should based on git

--- res/version_updater.py
+++ res/version_updater.py
@@ -7,10 +7,18 @@ def get_git_commit_count():
         return int(subprocess.check_output(["git", "rev-list", "HEAD", "--count"]).decode('utf-8').strip())
     except Exception:
         return -1
+    
+
+def get_last_commit_date():
+    try:
+        return subprocess.check_output(["git", "log", "-1", "--format=%cd", "--date=short"]).decode('utf-8').strip()
+    except Exception:
+        return ""
 
 
 def write_version_info(dirpath):
     vercount = get_git_commit_count()
+    verdate = get_last_commit_date()
     if vercount > -1:
         jspath = os.path.join(dirpath, "version.js")
         with open(jspath, "w", encoding="utf-8") as f:
@@ -19,7 +27,7 @@ def write_version_info(dirpath):
             f.write('version = "{}";\n'.format(ver))
         txtpath = os.path.join(dirpath, "VERSION")
         with open(txtpath, "w", encoding="utf-8") as f:
-            f.write('{} {}'.format(ver, time.strftime("%Y-%m-%d")))
+            f.write('{} {}'.format(ver, verdate))
         return ver
     return ""
 
